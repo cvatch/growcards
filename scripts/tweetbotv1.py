8ed2a24bf59cbca936617401ee6cba61af0746f9
Colin <fairyqueen6615@gmail.com>
2019-10-02 21:22:34 -0400

    files

---
+++ scripts/tweetbotv1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import os
+from markovbot import MarkovBot
+tweetbot = MarkovBot()
+dirname = os.path.abspath(C:\Users\Colin\Documents\Growtye\bot\tweetbot\markovbot-master\Freud_Dream_Psychology.txt)
+book = os.path.join(dirname,'Freud_Dream_Psychology.txt')
+tweet.read(book)
+my_first_text - tweetbot.generate_text(25, seedword=['dream','psychoanalysis'])
+print("tweetbot says:")
+print(my_first_text)
+
